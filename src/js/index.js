fec6495462d4f526a03831c58a7e7666aadcee66
Matthew Fisher <matt.fisher@fermyon.com>
2022-06-29 11:06:33 -0700

    responsive design
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>

--- src/js/index.js
+++ src/js/index.js
@@ -17,3 +17,22 @@ window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (ev
 function setTheme(mode) {
 	document.documentElement.className = `theme-${mode}`;
 }
+
+new ClipboardJS('.copy');
+
+// navbar-burger expansion
+document.addEventListener('DOMContentLoaded', () => {
+	// Get all "navbar-burger" elements
+	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+	// Add a click event on each of them
+	$navbarBurgers.forEach( el => {
+		el.addEventListener('click', () => {
+			// Get the target from the "data-target" attribute
+			const target = el.dataset.target;
+			const $target = document.getElementById(target);
+			// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+			el.classList.toggle('is-active');
+			$target.classList.toggle('is-active');
+		});
+	});
+});
