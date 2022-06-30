dd336a3f7b26d70dfb058b86d4c7a9f3dacec7ef
bacongobbler <matt.fisher@fishworks.io>
2022-06-30 15:03:41 +0000

    Deploying to gh-pages from @ fermyon/hippo-themes@c859001c227560ac951388b291d29fc0e239267d 🚀

---
+++ index.d1b5e8f6.js
@@ -0,0 +1,2 @@
+function e(e){document.documentElement.className=`theme-${e}`}window.matchMedia("(prefers-color-scheme: dark)").matches?e("dark"):e("light"),window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",(a=>{a.matches?e("dark"):e("light")}));
+//# sourceMappingURL=index.d1b5e8f6.js.map
